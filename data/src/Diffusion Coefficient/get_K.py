671250cc7cd3d12f4fe42b33b3f3bc24bc1ac5b7
李力皮 <97717865+love-eason@users.noreply.github.com>
2026-01-11 16:22:03 +0800

    Update get_K.py

--- data/src/Diffusion Coefficient/get_K.py
+++ data/src/Diffusion Coefficient/get_K.py
@@ -14,12 +14,8 @@ def get_K(file,name,jpg):
         values = [float(s) for s in line.split()]
         X.append(values[0])
         Y.append(values[1])
-    plt.scatter(X, Y, marker='o',label='真实值')
+    plt.scatter(X, Y, marker='o',label='True Values')
 
-
-
-    '''拟合'''
-    # 需要拟合的函数
     def f_1(x, A,b):
         return A * x+b
     A1,b1= op.curve_fit(f_1, X, Y)[0]
@@ -34,8 +30,8 @@ def get_K(file,name,jpg):
         Z.append(x)
         y = A1 * x+b1
         H.append(y)
-    plt.plot(Z, H,color='red',label='拟合曲线')
-    plt.legend() # 显示label
+    plt.plot(Z, H,color='red',label='fitted curve')
+    plt.legend()
     # plt.xlabel("Angles (deg)")
     # plt.ylabel("Potntial")
 
@@ -50,7 +46,3 @@ file='1base.txt'
 name='1base'
 jpg='1base.jpg'
 get_K(file,name,jpg )
-'''
-0.0008847568569774664
-0.02847844759547282
-'''
